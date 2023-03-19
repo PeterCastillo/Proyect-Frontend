edf067d2d8b4b00ee605e7723db68f92897e5df0
PeterCastillo <peterjackcc@gmail.com>
2023-03-19 17:57:15 -0500

    SideBarOptions

---
+++ utils/sideBarOptions.tsx
@@ -0,0 +1,14 @@
+export const sideBarOptions = [
+    {
+        icon: "",
+        name: "Mantenimiento",
+        subMenu: [
+            {
+                icon: "",
+                name: "",
+                url: "",
+                acceso: true
+            }
+        ]
+    }
+]
