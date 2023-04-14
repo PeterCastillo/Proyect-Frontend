97208822c50456498acdf05dea16ffbbce5988b5
PeterCastillo <peterjackcc@gmail.com>
2023-04-14 17:22:38 -0500

    Table

--- app/mantenimiento/usuarios/page.tsx
+++ app/mantenimiento/usuarios/page.tsx
@@ -104,14 +104,14 @@ export default function Page() {
   const next = (component: number) => {
     const renders = refTabContainer.current.children;
     if(component == 1 || component == 2){
-      refTabContainer.current.style.height = `auto`
+      refTabContainer.current.style.height = `max-content`
     } else {
       refTabContainer.current.style.height = `100%`
     }
     for (let i = 0; i < renders.length; i++) {
       renders[i].style.height = `100%`;
       if (i != component) {
-        renders[i].style.height = `10rem`;
+        renders[i].style.height = `20rem`;
       }
     }
     const transformNumber = component * 100;
